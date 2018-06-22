5e5d532852672df5d658ec5fe35fc1674997df19
ale-ks <apetersone@gmail.com>
2018-06-22 11:03:22 +0200

    Update auth.js

--- fr/auth.js
+++ fr/auth.js
@@ -1,5 +1,5 @@
 module.exports = {
-    'auth/invalid-email': "E-mail n'est pas valide",
+    'auth/invalid-email': "L'adresse e-mail n'est pas valide",
     'auth/user-not-found': 'Utilisateur introuvable',
     'auth/wrong-password': 'Mot de passe erroné',
 };
